8e0b196663fc6e7edf88e541998eb71baccc5b46
labsesdt <labsesdt@gmail.com>
2020-03-12 18:07:34 +0800

    ddd

---
+++ f2.c
@@ -0,0 +1,24 @@
+#include <stdio.h> // f2.c
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int main()
+{
+    pid_t pid;
+    int status, i;
+    if (fork() == 0)
+    {
+        printf("child process pid =%d\n", getpid());
+        exit(5);
+    }
+    else
+    {
+        sleep(1);
+        printf("Parent process, wait for child...\n");
+        pid = wait(&status);
+        i = WEXITSTATUS(status);
+        printf("child's pid =%d . exit status=%d\n", pid, i);
+    }
+    return 0;
+}
